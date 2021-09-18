e26fc333530544602d0f119760f98ba7fe807118
julinuzzo19 <julinuzzo19@gmail.com>
2021-09-18 20:47:23 -0300

    arreglos visuales y cambios menores en javascript

--- Ejercicio 1/js/index.js
+++ Ejercicio 1/js/index.js
@@ -1,81 +1,69 @@
-
-window.onload = function() {  
+window.onload = function () {
   //document.getElementById('enviar').addEventListener("click", pulsar, false);
 
   document.getElementById('cancelar').onclick = () => {
     var resultado = window.confirm('Desea volver a la pagina anterior?');
-    if (resultado === true) {
-      window.location.href = '/index.html';
-  } else { 
-      
-  }
-} 
-   /*  document.getElementById('form-encuesta').onsubmit = (e) => {
+    if (resultado) {
+      window.location.href = '../index.html';
+    } else {
+    }
+  };
+  /*  document.getElementById('form-encuesta').onsubmit = (e) => {
       e.preventDefault();
       console.log("object")
     };
   document.getElementById('btn-enviar').onsubmit = (e) => {
       e.preventDefault();
        };  */
+};
+function captura() {
+  let nombre = document.getElementById('nombre').value;
+  let apellido = document.getElementById('apellido').value;
+  let email = document.getElementById('email').value;
+  let fecha = document.getElementById('fechanac').value;
+  let valoracion = document.getElementById('valoracion').value;
+  let sexo_checkbox = document.getElementsByName('sexo');
 
-   
+  //Verifico valor de sexo
+  let sexo;
+  if (sexo_checkbox[0].checked) {
+    sexo = sexo_checkbox[0].value;
+  } else if (sexo_checkbox[1].checked) {
+  } else {
+    sexo = sexo_checkbox[2].value;
+  }
 
-
-
+  if (nombre == '') {
+    alert('El nombre es obligatorio');
+    document.getElementById('nombre').focus;
+  } else if (apellido === '') {
+    alert('El apellido es obligatorio');
+    document.getElementById('apellido').focus;
+  } else if (sexo === '') {
+    alert('El sexo es obligatorio');
+    document.getElementById('sexo').focus;
+  } else if (email === '') {
+    alert('El email es obligatorio');
+    document.getElementById('email').focus;
+  } else if (fecha === '') {
+    alert('Ingresar fecha es obligatorio');
+    document.getElementById('fechanac').focus;
+  } else if (valoracion === '') {
+    alert('La valoracion es obligatorio');
+    document.getElementById('valoracion').focus;
+  } else {
+    alert(
+      nombre +
+        ' ' +
+        apellido + // el salto de linea no funciona aca \n para asi imprimir uno abajo del otro
+        ' ' +
+        sexo +
+        ' ' +
+        email +
+        ' ' +
+        fecha +
+        ' ' +
+        valoracion
+    );
+  }
 }
-function captura(){
-  var nombreest = document.getElementById("nomest").value;
-  var apellidoest = document.getElementById("apellido").value;
-  var sexoest = document.getElementById("sexo").value;
-  var emailest = document.getElementById("email").value;
-  var fechaest = document.getElementById("fechanac").value;
-  var valorest = document.getElementById("valoracion").value;
-
- if(nombreest ==""){
-  alert("El nombre es obligatorio");
-  document.getElementById("nomest").focus;
-  }
-  
-  else if(apellidoest ===""){
-    alert("El apellido es obligatorio");
-    document.getElementById("apellido").focus;
-
-  }
-  else if(sexoest ===""){
-    alert("El sexo es obligatorio");
-    document.getElementById("sexo").focus;
-
-  }
-  else if(emailest ===""){
-    alert("El email es obligatorio");
-    document.getElementById("email").focus;
-
-  }
-  else if(fechaest ===""){
-    alert("Ingresar fecha es obligatorio");
-    document.getElementById("fechanac").focus;
-
-  }
-  else if(valorest ===""){
-    alert("La valoracion es obligatorio");
-    document.getElementById("valoracion").focus;
-
-  }
-  else{
-    alert(nombreest+" "+ apellidoest // el salto de linea no funciona aca \n para asi imprimir uno abajo del otro
-    +" "+sexoest 
-    +" "+emailest
-    +" "+fechaest
-    +" "+valorest);
-  }
-
-
-
- 
-
- }
-
-  /* document.getElementById('cancelar').onclick = () => {
-    window.location.href = '/index.html';
-  };*/
-
