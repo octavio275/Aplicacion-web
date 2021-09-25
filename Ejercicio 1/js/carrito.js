a36876abc0b6e82ad6dedc5317639cde597e3285
julinuzzo19 <julinuzzo19@gmail.com>
2021-09-25 20:02:58 -0300

    agregado articulos en carrito

--- Ejercicio 1/js/carrito.js
+++ Ejercicio 1/js/carrito.js
@@ -1,171 +1,23 @@
-const productos = [
-    {
-        id: 1,
-        nombre: 'Pera De Boxeo Vintage Everlast',
-        precio: 5.499,
-        imagen: 'botines-futbol.jpg'
-    },
-    {
-        id: 2,
-        nombre: 'Botines De Futbol adidas Predator 20.4',
-        precio: 8.899,
-        imagen: 'cebolla.jpg'
-    },
-    {
-        id: 3,
-        nombre: 'Pelota de Basquet Molten Gr7',
-        precio: 5.899,
-        imagen: 'pelota-basquet.jpg'
-    }
+//
+// $(document).ready(() => {
+//     $('#imagen').click(function() {
+//         $('#imagen').css({opacity: 0.5});
+// });
 
+// });
+//
 
-];
+$('#btn-1').click(() => {
 
-let carrito = [];
-let total = 0;
-const DOMitems = document.querySelector('#items');
-const DOMcarrito = document.querySelector('#carrito');
-const DOMtotal = document.querySelector('#total');
-const DOMbotonVaciar = document.querySelector('#boton-vaciar');
-
-// Funciones
-
-/**
- * Dibuja todos los productos a partir de productos[]. 
- */
-function renderizarProductos() {
-    productos.forEach((info) => {
-        
-        const miNodo = document.createElement('div');
-        miNodo.classList.add('card', 'col-sm-4');
-       
-        const miNodoCardBody = document.createElement('div');
-        miNodoCardBody.classList.add('card-body');
-       
-        const miNodoTitle = document.createElement('h5');
-        miNodoTitle.classList.add('card-title');
-        miNodoTitle.textContent = info.nombre;
-      
-        const miNodoImagen = document.createElement('img');
-        miNodoImagen.classList.add('img-fluid');
-        miNodoImagen.setAttribute('src', info.imagen);
-       
-        const miNodoPrecio = document.createElement('p');
-        miNodoPrecio.classList.add('card-text');
-        miNodoPrecio.textContent = info.precio + '$';
-       
-        const miNodoBoton = document.createElement('button');
-        miNodoBoton.classList.add('btn', 'btn-primary');
-        miNodoBoton.textContent = '+';
-        miNodoBoton.setAttribute('marcador', info.id);
-        miNodoBoton.addEventListener('click', anyadirProductoAlCarrito);
-      
-        miNodoCardBody.appendChild(miNodoImagen);
-        miNodoCardBody.appendChild(miNodoTitle);
-        miNodoCardBody.appendChild(miNodoPrecio);
-        miNodoCardBody.appendChild(miNodoBoton);
-        miNodo.appendChild(miNodoCardBody);
-        DOMitems.appendChild(miNodo);
-    });
-}
-
-/**
- * Evento para añadir un producto al carrito de la compra
- */
-function anyadirProductoAlCarrito(evento) {
-    // Anyadimos el Nodo a nuestro carrito
-    carrito.push(evento.target.getAttribute('marcador'))
-    // Calculo el total
-    calcularTotal();
-    // Actualizamos el carrito 
-    renderizarCarrito();
-
-}
-
-/**
- * Dibuja todos los productos guardados en el carrito
- */
-function renderizarCarrito() {
-    // Vaciamos todo el html
-    DOMcarrito.textContent = '';
-    // Quitamos los duplicados
-    const carritoSinDuplicados = [...new Set(carrito)];
-    // Generamos los Nodos a partir de carrito
-    carritoSinDuplicados.forEach((item) => {
-        // Obtenemos el item que necesitamos de la variable base de datos
-        const miItem = productos.filter((itemBaseDatos) => {
-            // ¿Coincide las id? Solo puede existir un caso
-            return itemBaseDatos.id === parseInt(item);
-        });
-        // Cuenta el número de veces que se repite el producto
-        const numeroUnidadesItem = carrito.reduce((total, itemId) => {
-            // ¿Coincide las id? Incremento el contador, en caso contrario no mantengo
-            return itemId === item ? total += 1 : total;
-        }, 0);
-        // Creamos el nodo del item del carrito
-        const miNodo = document.createElement('li');
-        miNodo.classList.add('list-group-item', 'text-right', 'mx-2');
-        miNodo.textContent = `${numeroUnidadesItem} x ${miItem[0].nombre} - ${miItem[0].precio}€`;
-        // Boton de borrar
-        const miBoton = document.createElement('button');
-        miBoton.classList.add('btn', 'btn-danger', 'mx-5');
-        miBoton.textContent = 'X';
-        miBoton.style.marginLeft = '1rem';
-        miBoton.dataset.item = item;
-        miBoton.addEventListener('click', borrarItemCarrito);
-        // Mezclamos nodos
-        miNodo.appendChild(miBoton);
-        DOMcarrito.appendChild(miNodo);
-    });
-}
-
-/**
- * Evento para borrar un elemento del carrito
- */
-function borrarItemCarrito(evento) {
-    // Obtenemos el producto ID que hay en el boton pulsado
-    const id = evento.target.dataset.item;
-    // Borramos todos los productos
-    carrito = carrito.filter((carritoId) => {
-        return carritoId !== id;
-    });
-    // volvemos a renderizar
-    renderizarCarrito();
-    // Calculamos de nuevo el precio
-    calcularTotal();
-}
-
-/**
- * Calcula el precio total teniendo en cuenta los productos repetidos
- */
-function calcularTotal() {
-    // Limpiamos precio anterior
-    total = 0;
-    // Recorremos el array del carrito
-    carrito.forEach((item) => {
-        // De cada elemento obtenemos su precio
-        const miItem = productos.filter((itemBaseDatos) => {
-            return itemBaseDatos.id === parseInt(item);
-        });
-        total = total + miItem[0].precio;
-    });
-    // Renderizamos el precio en el HTML
-    DOMtotal.textContent = total.toFixed(2);
-}
-
-/**
- * Varia el carrito y vuelve a dibujarlo
- */
-function vaciarCarrito() {
-    // Limpiamos los productos guardados
-    carrito = [];
-    // Renderizamos los cambios
-    renderizarCarrito();
-    calcularTotal();
-}
-
-// Eventos
-DOMbotonVaciar.addEventListener('click', vaciarCarrito);
-
-// Inicio
-renderizarProductos();
+    
+  $('#productos-carrito').append(`
+    <article class="article-carrito">
+        <img src="./img/raqueta-tenis.jpg" class="img-article-carrito" alt="pelota" />
+        <div class="container-article-carrito">
+        <span class="title-article-carrito">Raqueta de Tenis Wilson Federer 23 </span>
+        <span class="price-article-carrito">$5.499</span>
+        <button class="btn-article-carrito" type="button">Eliminar</button>
+        </div>
+    </article>   
+    `);
+});
