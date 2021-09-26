f59bda51dd66ba908190d6fa9cd718587b2f37e6
julinuzzo19 <julinuzzo19@gmail.com>
2021-09-26 01:20:50 -0300

    agregado logica para peticion http en pagina futbol

--- Ejercicio 1/js/carrito.js
+++ Ejercicio 1/js/carrito.js
@@ -1,23 +1,31 @@
-//
-// $(document).ready(() => {
-//     $('#imagen').click(function() {
-//         $('#imagen').css({opacity: 0.5});
-// });
+const removeArticle = (imagenId) => {
+  //   console.log(imagenId);
+  //   $(`#${imagenId}`).parent().css('background-color', 'white');
+  //   $(`#${imagenId}`).css({
+  //     opacity: '1'
+  //   });
+};
 
-// });
-//
+const addItemCarrito = () => {
+  event.stopImmediatePropagation();
+  console.log(event);
+  //   let imgId = data.id;
+  let imgId = event.target.id;
+  let articleId = $(`#${imgId}`).parent().attr('id');
 
-$('#btn-1').click(() => {
+  $(`#${imgId}`).parent().css('background-color', '#a3edb7');
 
-    
+  $(`#${imgId}`).css({
+    opacity: '0.6'
+  });
+
   $('#productos-carrito').append(`
-    <article class="article-carrito">
-        <img src="./img/raqueta-tenis.jpg" class="img-article-carrito" alt="pelota" />
-        <div class="container-article-carrito">
-        <span class="title-article-carrito">Raqueta de Tenis Wilson Federer 23 </span>
-        <span class="price-article-carrito">$5.499</span>
-        <button class="btn-article-carrito" type="button">Eliminar</button>
-        </div>
-    </article>   
-    `);
-});
+      <article class="article-carrito" id="article-carrito-${articleId}">
+          <img src="./img/raqueta-tenis.jpg" class="img-article-carrito" alt="pelota" />
+          <div class="container-article-carrito">
+            <span class="title-article-carrito">Raqueta de Tenis Wilson Federer 23 </span>
+            <span class="price-article-carrito">$5.499</span>     
+          </div>
+      </article>   
+      `);
+};
